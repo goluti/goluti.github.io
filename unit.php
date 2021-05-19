d7c2d3ff2989faa4d50686abc253885868979f2f
GOLU KUMAR TIWARI <84413458+goluti@users.noreply.github.com>
2021-05-19 19:45:58 +0530

    Create unit.php

---
+++ unit.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+</head>
+<body>
+<div class="container-fluid bg-dark">
+	<div class="row mt-2">
+		<div class="col-md-6">
+			<a href="SMS:gtiwari00004444@gmail.com" class="text-light mt-4" ><i class="fa fa-envelope" aria-hidden="true"> </i>gtiwari00004444@gmail.com</a>
+				<a href="tel:6204033826" class="text-white ml-4" ><i class="fa fa-phone" aria-hidden="true"></i>+916204033826</a>
+		</div>
+		<div class="col-md-6" style="margin-left: 1000px; margin-top: -20px;">
+			<a href="" class="text-success" style="font-size: 32px;" ><i class="fa fa-envelope" aria-hidden="true"></i></a>
+				<a href="https://touch.facebook.com/" class="text-primry" style="font-size: 32px;" ><i class="fab fa-facebook"></i></a>
+				<a href="https://twitter.com/LOGIN" class="text-primry"style="font-size: 32px;" ><i class="fab fa-twitter"></i></a>
+				<a href="https://telegram.org/" class="text-danger" style="font-size: 32px;" ><i class="fab fa-telegram"></i></a>
+				<a href="https://www.instagram.com/" class="text-danger" style="font-size: 32px;" ><i class="fab fa-instagram-square"></i></a>
+				<a href="https://www.youtube.com/" class="text-danger" style="font-size: 32px;" ><i class="fab fa-youtube"></i></a>
+		</div>
+
+		
+	</div>
+	
+</div>
+<nav class="navbar navbar-expand-md bg-light navbar-primary">
+
+  <a class="navbar-brand" href="#">
+  	<img src="img/un.png" style="width:100%; height: 70px;"></a>
+  
+  <ul class="navbar-nav " style="margin-left: 400px;">
+    <li class="nav-item ">
+      <a class="nav-link text-primary" href="#" style="font-size: 30px;">manu</a>
+      <h6 class="text-primary ml-2">star there</h6>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="about.php" style="font-size: 30px;">about</a>
+      <h6 class="text-primary ml-4">about US</h6>
+    </li><li class="nasv-item">
+      <a class="nav-link text-primary" href="sarvices.php" style="font-size: 30px;">services</a>
+      <h6 class="text-primary ml-4">our services</h6>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="contect.php" style="font-size: 30px;">contect</a>
+        <h6 class="text-primary ml-4">contect now</h6>
+    </li>
+ <li class="nav-item">
+      <a class="nav-link text-primary" href="portfolio.php" style="font-size: 30px;">portfolio</a>
+        <h6 class="text-primary ml-4 ">Your project</h6>
+    </li>
+    </li>
+
+  </ul>
+</nav>
+<div class="container-fluid">
+		<div class="row">
+		<div class="col-md-12">
+				<img src="img/soft.jpg" style="width: 100%; height: 500px; margin-top: 22px;">
+				<h1 class="text-primary bg-dark" style="width: 270px; margin-left:36%;">OUR SERVICES</h1>
+		</div>
+
+		</div>
+	</div>
+	<div class="container">
+	<div class="row mt-4">
+		<div class="col-md-4">
+			<img src="img/1.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail">
+			<h5 class="text-center">website desining</h5>
+			<p class="text-justify">In today’s techno world, the website plays an important role in profiting businesses and individuals. It gives them a ground to promote their ideas, products, services, and brands to the masses and thereby create a connectivity. So, if you are looking for reliable website designing</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/2.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">website devloapment</h5>
+			<p class="text-justify">best website development company in India, we offer our clients customizable web applications that will meet your business needs. No matter how complex the requirement is.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/3.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5>software devloapment</h5>
+			<p class="text-justify text-center">Software development is the process where applications, frameworks and other software components are developed. It also involves the designing, programming and testing of these components.
+
+</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/4.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">digitel marketing</h5>
+			<p class="text-justify">Digital Marketing also called online marketing or internet advertising , it is a form of marketing and advertising which uses the Internet to deliver promotional marketing messages to consumers.It includes SEO,SMO,PPC etc.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/5.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">SMS marketing</h5>
+			<p class="text-justify">Bulk SMS is highly preferable in between marketers or business managers because of the drastic increase in the usage of mobile phones in between young and old people belonging to different parts of the world.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/6.jpg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">wordpres devloapment</h5>
+			<p class="text-justify">we offer WordPress Development that compatible with all business. Our team of WordPress developers provides WordPress theme development,HTML to Wordpress Conversions, plugin development and theme customization services.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/E-commerce-development-company-in-patna.png" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">E_commerce devloapment</h5>
+			<p class="text-justify">An industry in which buying and selling of products or services takes place through electronic device is called E-commerce. E-commerce has gained huge and ultimate popularity among the business owners.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/8.jpeg" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">Graphic/Logo desining</h5>
+			<p class="text-justify">A logo is graphic mark,emblem,or symbol commonly used by commercial enterprises, organizations and even individuals to aid and promote instant public recognition. Logos are either purely graphic or are composed of the name of the organization.</p>
+		</div>
+		<div class="col-md-4">
+			<img src="img/7.png" style="width: 100%;height: 300px;"  class="img-thumbnail"><h5 class="text-center">Domene & hosting</h5>
+			<p class="text-justify">For any website wanting to make its presence in the internet world web hosting services is very important. Basically web hosting service is the internet hosting service through which people and companies get allowed to make their website accessible through World Wide Web.</p>
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row mt-4">
+		<div class="col-md-12">
+			<h2 class="text-primary mt-4" style=" margin-left: 500px;">what make US defrent</h2>
+<p class="text-justify mt-4" style="margin-left: 360px;" >With our approach to Web Design, 
+our methods continue to be proven in a continuous challenging <br> marketplace with delivering what we promise.</p>
+<div class="container">
+	<div class="row mt-4 sm">
+		<div class="col-md-4">
+			<img src="img/c.jpg" style="width: 100%; height: 600px; margin-left: 900px;">
+		</div>
+		</div>
+		<div style="">
+			<img src="img/i.png" style="height:60px;width: 60px; margin-top: -909px; margin-left: -73px;">
+			<h3 style="margin-top: -490px;">we are creative team</h3>
+				<p class="text-justify">We’ll give you brand new and original creative ideas to <br>
+				bring your campaigns to life and as always will throw in an abundance of friendly</p>
+		</div>
+		<div>
+			<img src="img/j.png" style="height:60px;width: 60px; margin-top: -609px; margin-left: -73px;">
+			<h3 style="margin-top: -334px;">Five-star support team</h3>
+			<p class="text-justify">Our Support Forum is at your disposal at any time, 5 Star Assistance is offered within 48 hours</p>
+		</div>
+		<div>
+			<img src="img/k.png" style="height:60px;width: 60px; margin-top: -309px; margin-left: -73px;">
+			<h3 style="margin-top: -180px;">Fully Integrated service</h3>
+			<p class="text-justify">As a leading digital agency we are able to offer a fully integrated service beyond the initial website design</p>
+		</div>
+		
+	</div>
+			
+		</div>
+	</div>
+</div>
+	<div class="container-fluid">
+		<div class="row mt-4">
+			<div class="col-md-12">
+				<img src="img/com.jpg" style="width: 100%;height: 500px;">
+				
+			</div>
+			
+		</div>
+	</div>
+	<div class="container-fluid bg-primary">
+		<div class="row mt-4">
+			<div class="col-md-12">
+				<h1 class="text-center">THINGS WE HAVE DONE</h1>
+				<h6 class="text-center">We are building bridges in web technology in order to connect the client’s goal to reality</h6>
+				<a href="" > <button type="button" class="btn btn-light hover" style="margin-left: 43%; margin-top: 34px; border-color:black;" >VIEW OUR PORTFOLIO</button></a>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mt-4">
+			<div class="col-md-12">
+				<h1 class="text-center">Lets Get Started
+your project</h1>
+<h6 class="text-center">We will help you to achieve your goals and to grow your business.</h6>
+<a href="" > <button type="button" class="btn btn-primary hover" style="margin-left: 43%; margin-top: 34px; border-color:black;" >REQUET A QUOTE</button></a>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mt-4">
+			<div class="col-md-6 text-primary text-center" style=" margin-top: 80px;">
+			<div class="jumbotron">
+  <h3 class=""><img src=""><i class="fa fa-phone" aria-hidden="true"></i> CALL US NOW</h3>
+  <p class="">Need help with your website? No problem!
+Our support team is here to help you
++91-8877552344 / +91-9117060606</p>
+<p>
+    <a class="btn btn-primary btn-sm" href="#" role="button">LET CALL US</a>
+  </p>
+</div>
+			</div>
+		<div class="col-md-6 text-primary text-center" style=" margin-top: 80px;">
+			<div class="jumbotron">
+  <h3 class=""><i class="fa fa-comments" aria-hidden="true"></i>ONLINE CHAT</h3>
+  <p class="">Need help with your website? No problem!
+Our support team is here to help you
++91-8877552344 / +91-9117060606</p>
+<p>
+    <a class="btn btn-primary btn-sm" href="#" role="button">GET STARTED</a>
+  </p>
+</div>
+			</div>
+		
+	</div>
+	<div class="container-fluid bg-dark " style="height: 200px">
+		<div class="row mt-4">
+			<div class="col-md-6">
+				<h6 class="text-light">© 2017 All Rights Reserved.</h6>
+			</div>
+			<div class="col-md-6">
+				<h6 class="text-light" style="margin-left: 930px; margin-top: 130px;">Designed & Developed by UNITE MICROSYSTEMS</h6>
+			</div>
+		</div>
+	</div>
+</body>
+</html>	
